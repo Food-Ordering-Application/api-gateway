5223df4c5269b2ee4c9446514ad8d5179231be46
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-17 17:56:16 +0700

    validate save pos order input

--- src/user/pos/dto/save-pos-order/pos-order-item-topping.dto.ts
+++ src/user/pos/dto/save-pos-order/pos-order-item-topping.dto.ts
@@ -1,9 +1,22 @@
+import { IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 import { State } from './../enums';
 export class PosOrderItemToppingDto {
+  @IsUUID()
   id?: string;
+
+  @IsUUID()
   toppingItemId: string;
+
+  @IsString()
   name: string;
+
+  @IsNumber()
   quantity: number;
+
+  @IsNumber()
+  @IsOptional()
   price: number;
+
+  @IsString()
   state: State;
 }
