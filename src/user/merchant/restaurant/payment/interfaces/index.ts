03ddead0886f4b485ceb5566f6350cd9a7e29693
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-20 17:27:38 +0700

    add payment response from user service

---
+++ src/user/merchant/restaurant/payment/interfaces/index.ts
@@ -0,0 +1,3 @@
+export { IFetchPaymentData } from './fetch-payment-data.interface';
+export { IUserServiceFetchPaymentOfRestaurantResponse } from './user-service-fetch-payment-of-restaurant-response.interface';
+export { IUserServiceAddPaypalPaymentResponse } from './user-service-add-paypal-payment-response.interface';
