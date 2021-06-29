441df6cae9490f51466b3f7777c54f4097556720
hienhhcc <thachdau16t@gmail.com>
2021-06-30 00:41:03 +0700

    Update pagination.dto.ts

--- src/shared/dto/pagination.dto.ts
+++ src/shared/dto/pagination.dto.ts
@@ -1,12 +1,14 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNumberString, IsString } from 'class-validator';
+import { IsNumberString, IsOptional, IsString } from 'class-validator';
 
 export class PaginationDto {
   @ApiPropertyOptional({ example: '0', required: false, default: '0' })
+  @IsOptional()
   @IsNumberString()
   page?: string;
 
   @ApiPropertyOptional({ example: '10', required: false, default: '10' })
+  @IsOptional()
   @IsNumberString()
   size?: string;
 }
