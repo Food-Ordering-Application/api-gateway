7b9fa87d6191ad7d77366eeb1a1f79fba9ca3d6b
hienhhcc <thachdau16t@gmail.com>
2021-06-29 21:33:23 +0700

    update

--- src/user/admin/dto/verify-restaurant/verify-restaurant.dto.ts
+++ src/user/admin/dto/verify-restaurant/verify-restaurant.dto.ts
@@ -1,11 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsUUID } from 'class-validator';
 
 export class VerifyRestaurantDto {
   @ApiProperty({
     example: '5b866b5f-d32a-440a-b230-ac0dd7ff9157',
     required: true,
   })
-  @IsString()
+  @IsUUID()
   restaurantId: string;
 }
--- src/user/customer/dto/create-customer/create-customer.dto.ts
+++ src/user/customer/dto/create-customer/create-customer.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNumberString, IsPhoneNumber, IsString } from 'class-validator';
 
 export class CreateCustomerDto {
   @ApiProperty({ example: '0123456789', uniqueItems: true, required: true })
-  @IsString()
+  @IsPhoneNumber('VN')
   phoneNumber: string;
   @ApiProperty({ minLength: 6, example: 'daylapasscuatui', required: true })
   @IsString()
